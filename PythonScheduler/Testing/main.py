70b31fb238eafa06a18eab232821fe8a655fbdff
Zack Dragos <zardragos@gmail.com>
2024-11-15 17:26:56 -0500

    Test commit showing Sam

--- PythonScheduler/Testing/main.py
+++ PythonScheduler/Testing/main.py
@@ -3,7 +3,6 @@
 
 from PythonScheduler.Classes.Horse import *
 from PythonScheduler.Classes.Rider import *
-from PythonScheduler.Classes.Daily_Schedule import *
 from PythonScheduler.Classes.Weekly_Schedule import *
 
 
@@ -12,7 +11,10 @@ def main():
     Schedule.add_horse(Horse("Kitty"))
     Schedule.add_horse(Horse("Spotty"))
     Schedule.add_rider(Rider("Sammy", None, 60, 150, 0, None, 0))
-
+    for horse in Schedule.get_horses():
+        print(horse)
+    for rider in Schedule.get_riders():
+        print(rider)
 
 if __name__ == "__main__":
     main()
